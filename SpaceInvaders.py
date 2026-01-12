726a0a12a7e2852023d22e4b930f672c65eca3e5
elon-mouse <142508817+elon-mouse@users.noreply.github.com>
2026-01-12 19:33:19 +0000

    Changed the movement to use a loop

--- SpaceInvaders.py
+++ SpaceInvaders.py
@@ -18,29 +18,31 @@ ship.penup()
 for i in range(7):
     enemy = turtle.Turtle()
     enemy.showturtle()
-    enemy.hideturtle()
     enemy.speed(0)
     enemy.shape("circle")
     enemy.penup()
     enemy.color("grey")
-    enemy.goto(-30+i,30+i)
+    enemy.goto(-30*i,30*i)
 
 
-laser = turtle.Turtle()
-laser.color("red")
-laser.shape("circle")
-laser.left(90)
-laser.penup()
-laser.hideturtle()
+
+list1=[]
 
 def bullet():
+    laser = turtle.Turtle()
+    laser.hideturtle()
+    laser.color("red")
+    laser.shape("circle")
+    laser.left(90)
+    laser.penup()
+    list1.append(laser)
     x = ship.xcor()
     y = ship.ycor()
     laser.speed(0)
     laser.goto(x,y)
-    laser.speed(2)
     laser.showturtle()
-    laser.forward(10000)
+    laser.speed(16)
+
     
 def right():
     x = ship.xcor()
@@ -57,6 +59,20 @@ wn.onkeypress(bullet, "space")
 wn.onkeypress(right, "Right")
 wn.onkeypress(left, "Left")
 
+keepgoing = True
+def gameloop():
+    global keepgoing 
+    for b in list1:
+        b.showturtle()
+        b.forward(5)
 
+
+
+
+    wn.update() 
+    if keepgoing: 
+        wn.ontimer(gameloop, 16)
+
+gameloop()
 wn.listen()
 wn.mainloop()
