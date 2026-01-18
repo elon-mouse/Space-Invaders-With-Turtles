43c79c091cb48a1947e21dd83b99f9c0a2931a71
elon-mouse <142508817+elon-mouse@users.noreply.github.com>
2026-01-18 04:19:19 +0000

    now the enemys attack, the player can die the enemys attacks, and I imported back random

--- SpaceInvaders.py
+++ SpaceInvaders.py
@@ -1,5 +1,5 @@
 import turtle
-
+import random
 
 wn = turtle.Screen()
 ship = turtle.Turtle()
@@ -16,6 +16,7 @@ text = turtle.Turtle()
 text.hideturtle()
 text.color("white")
 text.penup()
+text.goto(0,0)
 
 list2=[]
 
@@ -40,6 +41,30 @@ for i in range(6):
         list2.append(enemy)
         enemy.color("grey")
         enemy.goto(30*i,30*j)
+
+list3=[]
+
+enemyCooldown = False
+def resetEnemyCooldown():
+    global enemyCooldown
+    enemyCooldown = False
+
+def enemyShoot():
+    global enemyCooldown
+    if enemyCooldown == False and len(list2) > 0:
+        enemyCooldown = True
+        wn.ontimer(resetEnemyCooldown, 1000)
+        enemyshoot = random.choice(list2)
+        bullet = turtle.Turtle()
+        bullet.hideturtle()
+        bullet.color("green")
+        bullet.shape("circle")
+        bullet.penup()
+        bullet.speed(0)
+        bullet.goto(enemyshoot.xcor(), enemyshoot.ycor())
+        bullet.showturtle()
+        list3.append(bullet)
+
 
 list1=[]
 
@@ -76,7 +101,7 @@ def enemymovedown():
     global cooldown1
     if cooldown1 == False:
         cooldown1 = True
-        wn.ontimer(changeCooldown1, 20000) 
+        wn.ontimer(changeCooldown1, 20000)
         for e in list2:
             x = e.xcor()
             y = e.ycor()
@@ -97,7 +122,9 @@ wn.score=0
 wn.title("Space Invaders. Score: {0}".format(wn.score))
 
 wn.onkeypress(right, "Right")
+wn.onkeypress(right, "d")
 wn.onkeypress(left, "Left")
+wn.onkeypress(left, "a")
 wn.onkeypress(bullet, "space")
 
 keepgoing = True
@@ -105,28 +132,48 @@ def gameloop():
     wn.title("Space Invaders. Score: {0}".format(wn.score))
     global keepgoing
     enemymovedown()
+    enemyShoot()
 
-    for b in list1:              
+    for eb in list3[:]:
+        eb.goto(eb.xcor(), eb.ycor()-15)
+
+        if eb.ycor() < -540:
+            eb.hideturtle()
+            list3.remove(eb)
+            
+
+        if eb.distance(ship) < 20:
+            keepgoing = False
+            eb.hideturtle()
+            text.clear()
+            text.write("GAME OVER",align="center",font=("Arial", 48, "bold"))
+            wn.title("GAME OVER")
+            break
+
+    for b in list1[:]:
         b.showturtle()
         b.goto(b.xcor(),b.ycor()+20)
 
-        if b.ycor() > 540:                 
+        if b.ycor() > 540:
             b.hideturtle()
             list1.remove(b)
+            
 
-        for e in list2:                 
-            if b.distance(e) < 15:         
+        for e in list2[:]:
+            if b.distance(e) < 15:
                 wn.score+=1
                 e.hideturtle()
                 list2.remove(e)
                 b.hideturtle()
                 list1.remove(b)
-            if len(list2) == 0:
-                keepgoing = False
-                text.write(
-                    "YOU WIN!\nFinal Score: {0}".format(wn.score), align="center", font=("Arial", 48, "bold"))
-                wn.title("YOU WIN! Final Score: {0}".format(wn.score))
+
+                if len(list2) == 0:
+                    keepgoing = False
+                    text.clear()
+                    text.write("YOU WIN!\nFinal Score: {0}".format(wn.score), align="center", font=("Arial", 48, "bold"))
+                    wn.title("YOU WIN! Final Score: {0}".format(wn.score))
                 break
+
     wn.update()
     if keepgoing:
         wn.ontimer(gameloop, 16)
